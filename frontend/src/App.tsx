59fe51a119f6e6a706f5ea063811fc0b90525bd9
saba farzanegan <saba.far.99@gmail.com>
2025-05-22 20:51:00 +0330

    handle signup form and action

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,48 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Setting from "./pages/Setting";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "./store/useAuthStore";
+import { Toaster } from "sonner";
 
 function App() {
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
+  useQuery({
+    queryKey: ["user"],
+    queryFn: checkAuth,
+  });
+  console.log(authUser);
+
+  if (!authUser && isCheckingAuth) {
+    return (
+      <div className="w-full h-svh grid place-content-center">
+        <span className="loading loading-ring loading-xl"></span>
+      </div>
+    );
+  }
   return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/Signup" element={<Signup />} />
-    </Routes>
+    <div data-theme="">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/setting"
+          element={authUser ? <Setting /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={authUser ? <Home /> : <Login />} />
+        <Route path="/Signup" element={authUser ? <Home /> : <Signup />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
